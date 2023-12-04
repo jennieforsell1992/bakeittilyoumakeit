15aa8dfcb6c3a4ffbc214178ad99d80a6e799bf2
Jennie <forselljennie@gmail.com>
2023-12-04 16:11:15 +0100

    (feat): created a Router to all my pages

---
+++ frontend/src/Router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./components/pages/Layout";
+import { Home } from "./components/pages/Home";
+import { AllRecipes } from "./components/pages/AllRecipes";
+import { CreateNewRecipe } from "./components/pages/CreateNewRecipe";
+import { LikedRecipes } from "./components/pages/LikedRecipes";
+import { SearchRecipe } from "./components/pages/SeachRecipe";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      { path: "/", element: <Home></Home>, index: true },
+      { path: "/allrecipes", element: <AllRecipes></AllRecipes> },
+      {
+        path: "/createnewrecipe",
+        element: <CreateNewRecipe></CreateNewRecipe>,
+      },
+      { path: "/likedrecipe", element: <LikedRecipes></LikedRecipes> },
+      { path: "/searchrecipe", element: <SearchRecipe></SearchRecipe> },
+    ],
+  },
+]);
