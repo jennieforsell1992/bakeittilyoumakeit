f10f6b0a76754e44e727d1c682d139905e3e357d
Jennie <forselljennie@gmail.com>
2023-12-04 16:15:41 +0100

    (feat): AllRecipes page implemented a useEffect to get the data from my service-file. Added providers in my return

--- frontend/src/components/pages/AllRecipes.tsx
+++ frontend/src/components/pages/AllRecipes.tsx
@@ -1,3 +1,33 @@
+import { useEffect, useReducer } from "react";
+import { RecipeDispatchContext } from "../../contexts/RecipeDispatchContext";
+import { RecipeReducer, ActionType } from "../../reducers/RecipeReducer";
+import { ShowAllRecipes } from "../ShowAllRecipes";
+import { getAllRecipes } from "../../services/RecipeApi";
+import { RecipeContext } from "../../contexts/RecipeContext";
+
 export const AllRecipes = () => {
-  return <p>Alla recept</p>;
+  const [recipes, dispatch] = useReducer(RecipeReducer, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      const getAllDataFromApi = await getAllRecipes();
+
+      dispatch({
+        type: ActionType.GOTALLRECIPES,
+        payload: JSON.stringify(getAllDataFromApi),
+      });
+    };
+
+    getData();
+  }, [dispatch]);
+
+  return (
+    <>
+      <RecipeContext.Provider value={recipes}>
+        <RecipeDispatchContext.Provider value={dispatch}>
+          <ShowAllRecipes></ShowAllRecipes>
+        </RecipeDispatchContext.Provider>
+      </RecipeContext.Provider>
+    </>
+  );
 };
