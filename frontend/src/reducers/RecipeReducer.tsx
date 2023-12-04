89fc4f1f1c1b95792b1de0702a5b39009da49734
Jennie <forselljennie@gmail.com>
2023-12-04 16:13:37 +0100

    (feat): created a reducer and one case - get all recipes.

---
+++ frontend/src/reducers/RecipeReducer.tsx
@@ -0,0 +1,26 @@
+import { IRecipe } from "../models/IRecipe";
+
+export interface IAction {
+  type: ActionType;
+  payload: any;
+}
+
+export enum ActionType {
+  GOTALLRECIPES,
+  CREATENEWRECIPE,
+}
+
+export const RecipeReducer = (state: IRecipe[], action: IAction) => {
+  switch (action.type) {
+    case ActionType.GOTALLRECIPES: {
+      return JSON.parse(action.payload);
+    }
+    case ActionType.CREATENEWRECIPE: {
+      return;
+    }
+    default:
+      break;
+  }
+
+  return state;
+};
