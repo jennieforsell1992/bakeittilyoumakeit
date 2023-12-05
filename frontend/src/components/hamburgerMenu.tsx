af2f177044855181084fdb5ecb15e3b1b0059b4d
Jennie <forselljennie@gmail.com>
2023-12-05 21:00:54 +0100

    (feat): added useState and a toggle-function to it. added classnames and a ternary in hamburger-menu class, if it open or not

--- frontend/src/components/hamburgerMenu.tsx
+++ frontend/src/components/hamburgerMenu.tsx
@@ -1,3 +1,32 @@
-export const hamburgerMenu = () => {
-  return <p>hamburger</p>;
+import { useState } from "react";
+
+export const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const links = [
+    { text: "Alla recept", url: "/allrecipes" },
+    { text: "Lägg till nytt recept", url: "/createnewrecipe" },
+  ];
+
+  return (
+    <div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
+      <div className="hamburger-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <ul className={`links ${isOpen ? "open" : ""}`}>
+        {links.map((link, index) => (
+          <li className="li-item" key={index}>
+            <a href={link.url}>{link.text}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
