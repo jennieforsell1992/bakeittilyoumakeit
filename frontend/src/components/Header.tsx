ea78ab813e4f9baf32e32b41a259f43ffc8b3fa1
Jennie <forselljennie@gmail.com>
2023-12-05 21:01:24 +0100

    (fix): moved Header to a hamburgerMenu component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,34 +1,19 @@
-import { useState } from "react";
 import "../scss/header.scss";
+import { HamburgerMenu } from "./hamburgerMenu";
 
 export const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const links = [
-    { text: "Alla recept", url: "/allrecipes" },
-    { text: "Lägg till nytt recept", url: "/createnewrecipe" },
-  ];
-
   return (
-    <div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
-      <div className="hamburger-icon" onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
+    <header className="header">
+      <p className="logo">Logo</p>
+      <div className="header-wrapper">
+        <div className="icon-wrapper-search">
+          <span className="material-symbols-outlined">search</span>
+        </div>
+        <div className="icon-wrapper-heart">
+          <span className="material-symbols-outlined">favorite</span>
+        </div>
+        <HamburgerMenu />
       </div>
-
-      {/* Länkar som visas/döljs beroende på isOpen */}
-      <ul className={`links ${isOpen ? "open" : ""}`}>
-        {links.map((link, index) => (
-          <li key={index}>
-            <a href={link.url}>{link.text}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    </header>
   );
 };
