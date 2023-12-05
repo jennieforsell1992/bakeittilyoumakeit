cee050e0b94191a1662c8b96774ae8b1d30396c4
Jennie <forselljennie@gmail.com>
2023-12-05 20:59:19 +0100

    (fix): added classnames to div and p-tags

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,14 @@
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div>
-        <p>Kontakt</p>
-        <p>Adress</p>
-        <p>telefonnummer</p>
-        <p>email</p>
-        <p>copywright?</p>
+      <div className="wrapper-contacts">
+        <p className="contact">Kontakt</p>
+        <p className="contact-text">
+          Adress: Gästrikegatan 11, 113 62 Stockholm
+        </p>
+        <p className="contact-text">telefonnummer: 0736516546</p>
+        <p className="contact-text">email: info@bakeittilyoumakeit.se</p>
+        <p className="contact-text">copywright?</p>
       </div>
     </footer>
   );
