693b15b126ea7bb0afc5b9c3d3e08d3782cf5f03
Jennie <forselljennie@gmail.com>
2023-12-06 14:19:17 +0100

    (feat): created Toggle-function to my likedRecipe in reducer and did a dispatch in showAllRecipes.tsx, added id in my interface IRecipe

--- frontend/src/components/ShowAllRecipes.tsx
+++ frontend/src/components/ShowAllRecipes.tsx
@@ -1,27 +1,61 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { RecipeContext } from "../contexts/RecipeContext";
-// import { RecipeDispatchContext } from "../contexts/RecipeDispatchContext";
-// import { ActionType } from "../reducers/RecipeReducer";
+import "../scss/showAllRecipes.scss";
+import { RecipeDispatchContext } from "../contexts/RecipeDispatchContext";
+import { ActionType } from "../reducers/RecipeReducer";
 
 export const ShowAllRecipes = () => {
   const allRecipes = useContext(RecipeContext);
-  //   const dispatch = useContext(RecipeDispatchContext);
+  const dispatch = useContext(RecipeDispatchContext);
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likedHeartToggle = () => {
+    setIsLiked(!isLiked);
+    console.log(isLiked);
+  };
+
+  const handleClickHeart = (id: string) => {
+    setIsLiked(!isLiked);
+    console.log(isLiked);
+    console.log(id);
+    dispatch({
+      type: ActionType.TOGGLEHEART,
+      payload: id,
+    });
+  };
 
   console.log(allRecipes);
 
   const showAllRecipes = allRecipes.map((res, index) => {
     return (
       <div className="allRecipe-wrapper" key={index}>
-        <div>
-          <img src={res.imgUrl} alt={res.name} />
+        <div className="allRecipe-wrapper-img">
+          <img className="allRecipe-img" src={res.imgUrl} alt={res.name} />
         </div>
-        <div>
-          <p className="allRecipe-wrapper__name">{res.name}</p>
-          <p className="allRecipe-wrapper__name">{res.bakingTime}</p>
+        <div className="allRecipe-wrapper-nameWrapper">
+          <p className="allRecipe-name">{res.name}</p>
+          <p className="allRecipe-bakingTime">{res.bakingTime}</p>
+          <div
+            className="icon-wrapper-allRecipes"
+            onClick={() => {
+              handleClickHeart(res._id);
+            }}
+          >
+            <span
+              className={
+                res.likedRecipe
+                  ? "material-symbols-outlined liked"
+                  : "material-symbols-outlined"
+              }
+            >
+              favorite
+            </span>
+          </div>
         </div>
       </div>
     );
   });
 
-  return <div>{showAllRecipes}</div>;
+  return <div className="allRecipe-container">{showAllRecipes}</div>;
 };
