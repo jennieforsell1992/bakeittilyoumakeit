0a761832e6b8fd00b746482a11eed393cdd4ed8c
Jennie <forselljennie@gmail.com>
2023-12-06 17:12:29 +0100

    (feat): changed IRecipe and imported multer in controllers, changed mockdata and installed multer to this project.

--- backend/src/models/IRecipe.ts
+++ backend/src/models/IRecipe.ts
@@ -1,23 +1,31 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 import { IIngredients } from "./IIngredients";
 import { IDescription } from "./IDescription";
 
 export interface IRecipe {
   likedRecipe: boolean;
-  imgUrl: string;
+  // imgUrl: string;
   name: string;
   bakingTime: string;
   description: IDescription;
   allIngredients: IIngredients;
+  image: {
+    filename: string;
+    path: string;
+  };
 }
 
 export const recipeSchema = new Schema<IRecipe>({
   likedRecipe: { type: Boolean, required: true },
-  imgUrl: { type: String, required: true },
+  // imgUrl: { type: String, required: true },
   name: { type: String, required: true },
   bakingTime: { type: String, required: true },
   description: { type: {}, required: true },
   allIngredients: { type: {}, required: true },
+  image: {
+    filename: { type: String, required: true },
+    path: { type: String, required: true },
+  },
 });
 
 export const Recipe = model<IRecipe>("Recipe", recipeSchema);
