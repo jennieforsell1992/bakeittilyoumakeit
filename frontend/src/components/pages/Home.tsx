ca96cf226bfe7799323fc18a479427be78cdcb9f
Jennie <forselljennie@gmail.com>
2023-12-07 12:07:14 +0100

    (feat): created html in Home component and added classnames

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,3 +1,30 @@
+import PastryPic from "../../../public/pastry.png";
+
 export const Home = () => {
-  return <p>Hem</p>;
+  return (
+    <div>
+      <section className="front-picture-wrapper">
+        <div className="title-wrapper">
+          <span className="title-wrapper-text">
+            Välkommen till alla Sveriges olika bakningsrecept
+          </span>
+        </div>
+      </section>
+      <section className="front-recipe-wrapper">
+        <div className="front-recipe-week">
+          <p className="front-recipe-title">Veckans recept</p>
+          <span className="front-recipe-text">
+            Crossaint med glasyr och walnöt
+          </span>
+        </div>
+        <div className="front-recipe-container">
+          <img
+            className="front-recipe-img"
+            src={PastryPic}
+            alt="bild på kaka"
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
