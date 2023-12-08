0dfc1ae509fa8ccf0fbd4e00f97ed533abf236ac
Jennie <forselljennie@gmail.com>
2023-12-08 17:26:36 +0100

    (fix): changed my schema, deleted a object in mockdata, imported middleware in routes

--- backend/src/models/IRecipe.ts
+++ backend/src/models/IRecipe.ts
@@ -4,28 +4,20 @@ import { IDescription } from "./IDescription";
 
 export interface IRecipe {
   likedRecipe: boolean;
-  // imgUrl: string;
+  imgUrl: string;
   name: string;
   bakingTime: string;
   description: IDescription;
   allIngredients: IIngredients;
-  image: {
-    filename: string;
-    path: string;
-  };
 }
 
 export const recipeSchema = new Schema<IRecipe>({
-  likedRecipe: { type: Boolean, required: true },
-  // imgUrl: { type: String, required: true },
+  likedRecipe: { type: Boolean, required: false },
+  imgUrl: { type: String, required: false },
   name: { type: String, required: true },
-  bakingTime: { type: String, required: true },
-  description: { type: {}, required: true },
-  allIngredients: { type: {}, required: true },
-  image: {
-    filename: { type: String, required: true },
-    path: { type: String, required: true },
-  },
+  bakingTime: { type: String, required: false },
+  description: { type: {}, required: false },
+  allIngredients: { type: {}, required: false },
 });
 
 export const Recipe = model<IRecipe>("Recipe", recipeSchema);
--- backend/src/routes/recipeRoutes.ts
+++ backend/src/routes/recipeRoutes.ts
@@ -3,10 +3,11 @@ import {
   createNewRecipe,
   getAllRecipes,
 } from "../controllers/recipeControllers";
+import uploadMulterMiddleware from "../middleware/multer";
 
 const router: Router = Router();
 
-router.post("/", createNewRecipe);
+router.post("/", uploadMulterMiddleware, createNewRecipe);
 router.get("/", getAllRecipes);
 
 export default router;
