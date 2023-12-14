cf7817501e58ece323eec1318ea5e0afc4f06252
Jennie <forselljennie@gmail.com>
2023-12-14 16:07:31 +0100

    (feat): created a search funktion to my objects in context, imported useNavigate to navigate to ShowOneRecipe, imported handleClickFunction to heart-icon.

--- frontend/src/components/ShowSearchRecipe.tsx
+++ frontend/src/components/ShowSearchRecipe.tsx
@@ -1,14 +1,84 @@
-import { useContext } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import { RecipeContext } from "../contexts/RecipeContext";
+import { Link, useNavigate } from "react-router-dom";
 import { RecipeDispatchContext } from "../contexts/RecipeDispatchContext";
+import { ActionType } from "../reducers/RecipeReducer";
+import "../scss/showSearchRecipe.scss";
 
 export const ShowSearchRecipe = () => {
   const recipes = useContext(RecipeContext);
   const dispatch = useContext(RecipeDispatchContext);
+  const navigate = useNavigate();
+  const [searchRecipes, setSearchRecipes] = useState("");
+
+  const handleNavigateToOneRecipe = (id: string) => {
+    navigate(`/allrecipes/${id}`);
+  };
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setSearchRecipes(value);
+  };
+  console.log(searchRecipes);
+
+  const filteredRecipes = recipes.filter(
+    (rec) =>
+      searchRecipes === "" ||
+      rec.name.toLowerCase().includes(searchRecipes.toLowerCase())
+  );
+  const handleClickHeart = (id: string) => {
+    dispatch({
+      type: ActionType.TOGGLEHEART,
+      payload: id,
+    });
+  };
 
   return (
     <>
-      <input type="text" />
+      <input type="text" onChange={handleSearch} value={searchRecipes} />
+
+      {searchRecipes !== "" && (
+        <div>
+          {filteredRecipes.map((rec, index) => (
+            <div
+              onClick={() => {
+                handleNavigateToOneRecipe(rec._id);
+              }}
+              className="allRecipe-wrapper"
+              key={index}
+            >
+              <div className="allRecipe-wrapper-img">
+                <img
+                  className="allRecipe-img"
+                  src={rec.imgUrl}
+                  alt={rec.name}
+                />
+                <div
+                  className="icon-wrapper-allRecipes"
+                  onClick={() => {
+                    handleClickHeart(rec._id);
+                  }}
+                >
+                  <span
+                    className={
+                      rec.likedRecipe
+                        ? "material-symbols-outlined liked"
+                        : "material-symbols-outlined"
+                    }
+                  >
+                    favorite
+                  </span>
+                </div>
+              </div>
+              <div className="allRecipe-wrapper-nameWrapper">
+                <p className="allRecipe-name">{rec.name}</p>
+                <p className="allRecipe-bakingTime">{rec.bakingTime}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
