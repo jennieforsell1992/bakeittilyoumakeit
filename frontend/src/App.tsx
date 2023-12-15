c95555c69423062a242b4ec20da9b14311b9b45d
Jennie <forselljennie@gmail.com>
2023-12-15 10:29:42 +0100

    (feat): moved my ContextProvider to the root App.tsx component because of the DRY-rule.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,35 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
 import "./App.scss";
+import { useEffect, useReducer } from "react";
+import { ActionType, RecipeReducer } from "./reducers/RecipeReducer";
+import { getAllRecipes } from "./services/RecipeApi";
+import { RecipeContext } from "./contexts/RecipeContext";
+import { RecipeDispatchContext } from "./contexts/RecipeDispatchContext";
 
 function App() {
+  const [recipes, dispatch] = useReducer(RecipeReducer, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      const getAllDataFromApi = await getAllRecipes();
+
+      dispatch({
+        type: ActionType.GOTALLRECIPES,
+        payload: JSON.stringify(getAllDataFromApi),
+      });
+    };
+
+    getData();
+  }, [dispatch]);
+
   return (
     <>
-      <RouterProvider router={router} />
+      <RecipeContext.Provider value={recipes}>
+        <RecipeDispatchContext.Provider value={dispatch}>
+          <RouterProvider router={router} />
+        </RecipeDispatchContext.Provider>
+      </RecipeContext.Provider>
     </>
   );
 }
