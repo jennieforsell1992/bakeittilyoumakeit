04bd8df48186d723da1d2232228aeb526273546f
Jennie <forselljennie@gmail.com>
2023-12-18 13:25:26 +0100

    (feat): changed my origin to vercel

--- backend/app.ts
+++ backend/app.ts
@@ -18,7 +18,9 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:
+      "https://bakeittilyoumakeit-ohbk-jennie-forsells-projects.vercel.app",
     methods: ["GET", "POST"],
   })
 );
