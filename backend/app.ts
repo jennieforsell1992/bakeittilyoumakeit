9eacd3789c8ca32bf80d31b78d694af8918422f0
Jennie <forselljennie@gmail.com>
2023-12-22 13:56:12 +0100

    (fix): change url in backend

--- backend/app.ts
+++ backend/app.ts
@@ -18,9 +18,9 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    // origin:
-    //   "https://bakeittilyoumakeit-2ytc-7075yi68k-jennie-forsells-projects.vercel.app",
+    // origin: "http://localhost:5173",
+    origin:
+      "https://bakeittilyoumakeit-2ytc-7075yi68k-jennie-forsells-projects.vercel.app",
     methods: ["GET", "POST", "PUT"],
   })
 );
