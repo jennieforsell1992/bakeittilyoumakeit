2339aefd0a386df1ca2e3b9db09c927a044eeb61
Jennie <forselljennie@gmail.com>
2024-01-02 22:29:54 +0100

    (fix): delete port

--- backend/app.ts
+++ backend/app.ts
@@ -32,16 +32,16 @@ app.use((req, res) => {
   return;
 });
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 const mongoDbUrl = process.env.MONGODB_URI;
 async function run() {
   try {
     mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(mongoDbUrl || "");
 
-    app.listen(port, () => {
-      console.log(`server running on http://localhost:${port}`);
-    });
+    // app.listen(port, () => {
+    //   console.log(`server running on http://localhost:${port}`);
+    // });
   } catch (error) {
     console.log(error);
   }
