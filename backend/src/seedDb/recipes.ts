dd91759365377830a956908669f39e28b7c2dee2
Jennie <forselljennie@gmail.com>
2024-01-11 20:48:04 +0100

    (fix): added real recipes to mockdata

--- backend/src/seedDb/recipes.ts
+++ backend/src/seedDb/recipes.ts
@@ -1,90 +1,90 @@
 export const mockRecipesData = [
   {
     likedRecipe: false,
-    imgUrl: "../../public/uploads/imgUrl-1702285391215.jpg",
+    imgUrl:
+      "https://res.cloudinary.com/dwzwqnpmn/image/upload/v1704898465/recipe-images/7e2512f9-ad35-4442-aa58-e9f77f3892a1.jpg",
     name: "kladdkaka",
-    bakingTime: "20 minuter",
+    bakingTime: " 30 minuter",
     description: {
-      descriptionOne: "sätt på ugnen på 300grader",
-      descriptionTwo: "blanda ihop alla ingredientser",
-      descriptionThree: "lägg allting i en form",
-      descriptionFour: "grädda i ugnen 20 minuter",
-      descriptionFive: "ta ut och låt den svalna",
-      descriptionSix: "servera med grädde, hallon eller valfritt tillbehör",
+      descriptionOne: "Sätt ugnen på 175°.",
+      descriptionTwo:
+        "Smält smöret i en kastrull. Lyft av kastrullen från plattan.",
+      descriptionThree:
+        "Rör ner socker och ägg, blanda väl. Rör ner övriga ingredienser så att allt blir väl blandat.",
+      descriptionFour:
+        "Häll smeten i en smord och bröad form med löstagbar kant, ca 24 cm i diameter.",
+      descriptionFive:
+        "Grädda mitt i ugnen ca 15 min. Kakan blir låg med ganska hård yta och lite kladdig i mitten.",
+      descriptionSix:
+        "Låt kakan kallna. Pudra över florsocker. Servera med grädde eller glass och frukt.",
     },
     allIngredients: {
-      IngredientOne: "2dl mjöl",
-      IngredientTwo: "3dl mjölk",
-      IngredientThree: "3msk kakao",
-      IngredientFour: "100gram smör",
-      IngredientFive: "2tsk vaniljsocker",
+      IngredientOne: "100 gr smör",
+      IngredientTwo: "2½ dl strösocker",
+      IngredientThree: "2 ägg",
+      IngredientFour: "1 dl vetemjöl",
+      IngredientFive: "3 msk kakao",
+      IngredientSix: "1 tsk vaniljsocker",
+      IngredientSeven: "florsocker",
+      IngredientEight: "3 dl vispgrädde",
     },
   },
   {
     likedRecipe: false,
-    imgUrl: "../../public/uploads/imgUrl-1702295169721.jpg",
+    imgUrl:
+      "https://res.cloudinary.com/dwzwqnpmn/image/upload/v1704898838/recipe-images/b1b95c7c-9908-4917-9df2-f157e20453c2.jpg",
     name: "muffins",
-    bakingTime: "20 minuter",
+    bakingTime: "30 minuter",
     description: {
-      descriptionOne: "sätt på ugnen på 300grader",
-      descriptionTwo: "blanda ihop alla ingredientser",
-      descriptionThree: "lägg allting i en form",
-      descriptionFour: "grädda i ugnen 20 minuter",
-      descriptionFive: "ta ut och låt den svalna",
-      descriptionSix: "servera med grädde, hallon eller valfritt tillbehör",
+      descriptionOne: "Sätt ugnen på 225°.",
+      descriptionTwo:
+        "Sätt ut pappersformar på en plåt eller smörj en muffinsplåt lätt.",
+      descriptionThree:
+        "Smält smöret. Vispa ägg och socker pösigt. Tillsätt mjölk och smör.",
+      descriptionFour:
+        "Blanda mjöl och bakpulver och rör ner det i smeten. Tillsätt choklad eller bär. Klicka ut smeten i formarna.",
+      descriptionFive:
+        "Grädda i mitten av ugnen ca 20 min för stora och ca 12 min för normalstora.",
+      descriptionSix:
+        "Låt muffinsen svalna. Sikta över lite florsocker strax före servering.",
     },
     allIngredients: {
-      IngredientOne: "2dl mjöl",
-      IngredientTwo: "3dl mjölk",
-      IngredientThree: "3msk kakao",
-      IngredientFour: "100gram smör",
-      IngredientFive: "2tsk vaniljsocker",
+      IngredientOne: "50 gr smör",
+      IngredientTwo: "3 ägg",
+      IngredientThree: "2 dl strösocker",
+      IngredientFour: "1 dl mjölk",
+      IngredientFive: "4 dl vetemjöl",
+      IngredientSix: "2 tsk bakpulver",
+      IngredientSeven: "florsocker",
     },
   },
   {
     likedRecipe: false,
-    imgUrl: "../../public/uploads/imgUrl-1702295841413.jpg",
+    imgUrl:
+      "https://res.cloudinary.com/dwzwqnpmn/image/upload/v1704899936/recipe-images/742fe60f-f947-4545-b6d1-3041725cfc72.jpg",
     name: "kanelbullar",
-    bakingTime: "20 minuter",
+    bakingTime: "+60 minuter",
     description: {
-      descriptionOne: "sätt på ugnen på 300grader",
-      descriptionTwo: "blanda ihop alla ingredientser",
-      descriptionThree: "lägg allting i en form",
-      descriptionFour: "grädda i ugnen 20 minuter",
-      descriptionFive: "ta ut och låt den svalna",
-      descriptionSix: "servera med grädde, hallon eller valfritt tillbehör",
+      descriptionOne:
+        "Skär smöret i tunna skivor och lägg det i en bunke. Tillsätt 7 dl av mjölet, (spara ½ dl till utbakningen), socker, salt och jästen delad i mindre bitar.",
+      descriptionTwo:
+        "Värm mjölken till max 37° och rör ner i mjölblandningen. Arbeta degen blank och smidig. Låt jäsa övertäckt ca 30 min.",
+      descriptionThree:
+        "Sätt ugnen på 225°. Ta upp degen på mjölat bakbord och knåda den lätt. Kavla ut degen till en rektangulär platta, ca 25x50 cm. Bred ut smöret på degplattan. Strö över socker och kanel.",
+      descriptionFour:
+        "Rulla ihop från långsida till långsida till en rulle. Skär rullen i 20 bitar, ca 2 cm breda. Ställ bitarna med snittytan upp i pappersformar. Jäs kanelbullarna under bakduk ca 30 min.",
+      descriptionFive:
+        "Pensla kanelbullarna med ägg och strö över pärlsocker. Grädda i mitten av ugnen 6–8 min. Låt kanelbullarna svalna på galler.",
     },
     allIngredients: {
-      IngredientOne: "2dl mjöl",
-      IngredientTwo: "3dl mjölk",
-      IngredientThree: "3msk kakao",
-      IngredientFour: "100gram smör",
-      IngredientFive: "2tsk vaniljsocker",
-      IngredientSix: "100gram smör",
-      IngredientSeven: "2tsk vaniljsocker",
-    },
-  },
-  {
-    likedRecipe: false,
-    imgUrl: "bild",
-    name: "Hallongrotter",
-    bakingTime: "20 minuter",
-    description: {
-      descriptionOne: "sätt på ugnen på 300grader",
-      descriptionTwo: "blanda ihop alla ingredientser",
-      descriptionThree: "lägg allting i en form",
-      descriptionFour: "grädda i ugnen 20 minuter",
-      descriptionFive: "ta ut och låt den svalna",
-      descriptionSix: "servera med grädde, hallon eller valfritt tillbehör",
-    },
-    allIngredients: {
-      IngredientOne: "2dl mjöl",
-      IngredientTwo: "3dl mjölk",
-      IngredientThree: "3msk kakao",
-      IngredientFour: "100gram smör",
-      IngredientFive: "2tsk vaniljsocker",
-      IngredientSix: "100gram smör",
-      IngredientSeven: "2tsk vaniljsocker",
+      IngredientOne: "150 g smör",
+      IngredientTwo: "7½ dl vetemjöl",
+      IngredientThree: "½ dl strösocker",
+      IngredientFour: "2 krm salt",
+      IngredientFive: "25 g jäst",
+      IngredientSix: "2½ dl mjölk",
+      IngredientSeven: "2½ msk strösocker",
+      IngredientEight: "1½ msk malen kanel",
     },
   },
 ];
